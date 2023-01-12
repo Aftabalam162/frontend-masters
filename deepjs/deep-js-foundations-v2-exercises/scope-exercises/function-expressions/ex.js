33513ec017e9de75d9bb6d75e2f3462cd2be0936
aftabalam162 <itsaftabalam37@gmail.com>
2023-01-12 11:53:50 +0530

    solved all the problems in function-expression

--- deepjs/deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
+++ deepjs/deep-js-foundations-v2-exercises/scope-exercises/function-expressions/ex.js
@@ -2,8 +2,12 @@ function printRecords(recordIds) {
 	// TODO
 	
 	var studentData = recordIds.map( function getStudentData(studentId ) {
-		// getting each student through the map to return each student object
-		return studentRecords.find( function studentById (student) { return student.id == studentId })
+		
+		let studentRecord = studentRecords.find(function findStudent (student) {
+	    return student.id == studentId;
+	})
+		
+		return studentRecord;
 		// using built-in find function to return the obj with specific id
 	} );
 	
@@ -32,28 +36,145 @@ function printRecords(recordIds) {
 	});
 }
 
+
+
 function paidStudentsToEnroll() {
 	// TODO
 	
-	var studentsToEnroll = studentRecords.map( function paidNotEnroll(student) {
-		if (student.paid && !currentEnrollment.includes(student.id)) {
-			return student.id;
-		}
+	var studentsToEnroll = studentRecords.filter(
+        function getStudent(student) {
+            return student.paid == true && !currentEnrollment.includes(student.id);
+        }
+        // filtering through the list to find student who paid but yet to enroll
+        // it will return an object
+    )
+    
+	studentsToEnroll = studentsToEnroll.map( function getStudentID (record) {
+    		return record.id;
 	});
+	// mapped list of studentIDs
 	
-	var paidStudents = [...currentEnrollment, ...studentsToEnroll];
+    
+	studentsToEnroll = [...studentsToEnroll, ...currentEnrollment];
+	// included students who have enrolled
 	
-	paidStudents.forEach(function logEnrolledStudents(student) {
-		let studentRecord = studentRecords.find( function getStudentData( studentObj ) { return studentObj.id == student })
-		console.log(`${student.name} (${student.id}): ${student.paid? "Paid": "Not Paid"}`);
-	});
-	
-	
+	return studentsToEnroll;
 }
 
 function remindUnpaid(recordIds) {
 	// TODO
+	
+	var studentIDs = recordIds.filter( function unpaidStudents(studentID) {
+		let studentRecord =  studentRecords.find( function findStudent (student) {
+			return student.id == studentID;
+		});
+		// finding the student by their respective ID
+		
+		return !studentRecord.paid && currentEnrollment.includes(studentRecord.id);
+		// this will return students who didn't paid but enrolled
+	});
+	// 
+	
+	studentIDs.forEach( function remindStudents (studentID) {
+	    let studentRecord =  studentRecords.find( function findStudent (student) {
+			return student.id == studentID;
+		});
+		//this will return the student obj by corresponding ID
+		console.log(`${studentRecord.name} (${studentRecord.id}): ${studentRecord.paid? "Paid": "Not Paid"}`);
+	});
 }
+
+
+
+// ********************************
+
+// Refactoring code with Arrow Functions wherever possible
+
+function printRecords(recordIds) {
+	// TODO
+	
+	var studentData = recordIds.map( studentId => {
+		
+		let studentRecord = studentRecords.find(student => {
+	    return student.id == studentId;
+	})
+		
+		return studentRecord;
+		// using built-in find function to return the obj with specific id
+	} );
+	
+	studentData.sort((studentObj1, studentObj2) => { 
+		let studentName1 = studentObj1.name.toUpperCase();
+		let studentName2 = studentObj2.name.toUpperCase();
+		// this is done to avoid case issues
+		
+		if (studentName1 < studentName2) {
+			return -1;
+		}
+		
+		if (studentName1 > studentName2) {
+			return 1;
+		}
+		
+		return 0;
+		
+		// Read more about array of objects here: 
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+		
+	});
+	
+	studentData.forEach(student => {
+		console.log(`${student.name} (${student.id}): ${student.paid? 'Paid': 'Not Paid'}`);	
+	});
+}
+
+function paidStudentsToEnroll() {
+	// TODO
+	
+	var studentsToEnroll = studentRecords.filter(
+        student => {
+            return student.paid == true && !currentEnrollment.includes(student.id);
+        }
+        // filtering through the list to find student who paid but yet to enroll
+        // it will return an object
+    )
+    
+	studentsToEnroll = studentsToEnroll.map( record => {
+    		return record.id;
+	});
+	// mapped list of studentIDs
+	
+    
+	studentsToEnroll = [...studentsToEnroll, ...currentEnrollment];
+	// included students who have enrolled
+	
+	return studentsToEnroll;
+}
+
+function remindUnpaid(recordIds) {
+	// TODO
+	
+	var studentIDs = recordIds.filter( studentID => {
+		let studentRecord =  studentRecords.find(student => {
+			return student.id == studentID;
+		});
+		// finding the student by their respective ID
+		
+		return !studentRecord.paid && currentEnrollment.includes(studentRecord.id);
+		// this will return students who didn't paid but enrolled
+	});
+	// 
+	
+	studentIDs.forEach( studentID => {
+	    let studentRecord =  studentRecords.find( student => {
+			return student.id == studentID;
+		});
+		//this will return the student obj by corresponding ID
+		console.log(`${studentRecord.name} (${studentRecord.id}): ${studentRecord.paid? "Paid": "Not Paid"}`);
+	});
+}
+
+
 
 
 // ********************************
